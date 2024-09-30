d7ad28961fdb6739ef1bb851f0defdc7d5fbc945
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-09-30 16:55:04 -0300

    Add files via upload

--- atvd8/main.py
+++ atvd8/main.py
@@ -1,10 +1,8 @@
-nota1= float(input("insira nota 1:"))
-nota2= float(input("insira nota 2:"))
-nota3= float(input("insira nota 3:"))
-nota4= float(input("insira nota 4:"))
-nota5= float(input("insira nota 5:"))
+soma_notas = 0
 
-soma_notas = nota1 + nota2 + nota3 + nota4 + nota5
+for i in range(5):
+    nota = float(input(f"Digite a nota {i + 1}: "))
+    soma_notas += nota
 media = soma_notas / 5
 
 print(f"A média das notas é: {media:.2f}")
