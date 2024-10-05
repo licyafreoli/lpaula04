f536ae09d18716e2c95a1d8a904b7744a0b241df
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-10-05 14:17:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-positivos= 0
-negativos= 0
 
-for i in range(10):
-    numero = float(input(f"digite o {i+1}° número:"))
-    if numero >0:
-     positivos +=1
-    elif numero <0:
-     negativos +=1
-print(f"números positivos: {positivos}")
-print(f"números negativos: {negativos}")
